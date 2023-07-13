850198cbe7393650010f9119efa78d159064d877
Vaishali Vaidya <vaidyavaishaliabc12@gmail.com>
2023-07-13 23:01:14 +0530

    ws url added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Routes from "./Routes";
 function App() {
   axios.defaults.baseURL = import.meta.env.VITE_SERVER_URL;
   console.log("server_url="+import.meta.env.VITE_SERVER_URL)
+  console.log("ws_url="+import.meta.env.VITE_WS)
   axios.defaults.withCredentials = true;
   return (
     <UserContextProvider>
